cc43ff1481e54721993ee0e3fdb6231faa2c6003
Matu36 <matipineda85@live.com.ar>
2022-08-02 22:09:48 -0300

    binarios

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,13 +1,20 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
-}
+  let suma = 0;
+  var numReverse = num.split("").reverse().join ("");
+  for (var i = 0; i < numReverse.length; i++) {
+      suma = suma + numReverse[i] * 2 ** i};
+  return suma;
+} 
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  var numeroBinario = [];
+    var numeroPorDividir = num;
+    for (;num;) {
+        numeroBinario.unshift(num%2)
+        var num = Math.floor(num / 2)}
+    return numeroBinario.join ("");
 }
 
 
