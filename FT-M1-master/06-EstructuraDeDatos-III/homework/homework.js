da0f91a71cc81b729a448fdc315e147dc8728e4d
Matu36 <matipineda85@live.com.ar>
2022-08-22 06:54:03 -0300

    homework6

--- FT-M1-master/06-EstructuraDeDatos-III/homework/homework.js
+++ FT-M1-master/06-EstructuraDeDatos-III/homework/homework.js
@@ -11,7 +11,87 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+function BinarySearchTree (value) {
+this.value = value;
+this.right = null;
+this.left = null
+}
+
+BinarySearchTree.prototype.insert = function (valueToInsert) {
+  if (this.value > valueToInsert) {
+if (!this.left) {
+  this.left = new BinarySearchTree (valueToInsert);
+} else {
+    this.left.insert (valueToInsert); 
+  };
+}
+    else {
+    if (!this.right) {
+      this.right = new BinarySearchTree (valueToInsert);}
+else {
+  this.right.insert (valueToInsert);
+};
+    };
+  };
+
+
+
+/* Hacerlo con recursion -->      function suma () let count = 0;
+  return function () {
+    return count++};
+*/
+
+BinarySearchTree.prototype.contains = function (value) {
+if (this.value === value) return true;
+
+if (this.left) {
+if (this.left.contains (value)) return true;
+}
+if (this.right) {
+  if (this.right.contains (value)) return true;
+}
+return false;
+};
+
+BinarySearchTree.prototype.depthFirstForEach = function (cb, 
+  order = "in-order") {
+switch (order) {
+case "in-order": {
+  this.left?.depthFirstForEach (cb, order);
+  cb (this.value);
+  this.right?.depthFirstForEach (cb, order);
+  break;
+}
+case "pre-order": {
+  cb (this.value);
+  this.left?.depthFirstForEach (cb, order);
+  this.right?.depthFirstForEach (cb, order);
+  break;
+}
+case "post-order": {
+  this.left?.depthFirstForEach (cb, order);
+  this.right?.depthFirstForEach (cb, order);
+  cb (this.value);
+  break;
+}
+}
+  };
+
+BinarySearchTree.prototype.breadthFirstForEach = function (cb, queue = []) {
+cb (this.value);
+if (this.left) queue.push (this.left)
+if (this.right) queue.push (this.right);
+if (queue.length) {queue.shift(). breadthFirstForEach (cb, queue)}
+};
+
+BinarySearchTree.prototype.size = function () {
+  let count = 1;
+  if (this.left) count += this.left.size ();
+  if (this.right) count += this.left.size ();
+  return count;
+};
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
