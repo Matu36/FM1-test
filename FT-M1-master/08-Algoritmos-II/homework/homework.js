988dafee2077d4a4fd016518e7ae80b93ce63dcc
Matu36 <matipineda85@live.com.ar>
2022-08-26 14:47:20 -0300

    homework8

--- FT-M1-master/08-Algoritmos-II/homework/homework.js
+++ FT-M1-master/08-Algoritmos-II/homework/homework.js
@@ -6,16 +6,48 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+if (array.length <= 1) return array;
 
+const left = [],
+right = [],
+pivot = array.shift();
+
+for (let item of array) {
+  pivot > item ? left.push (item) : right.push (item);
 }
+
+return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+
 
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+if (array.length <= 1) return array;
 
+const i = Math.floor(array.length / 2);
+let left = array.slice (0, i);
+let right = array.slice (i);
+
+left = mergeSort (left);
+right = mergeSort (right);
+
+const result = [];
+
+while (left.length && right.length) {
+if (left [0] > right[0]) {
+result.push(right.shift())
 }
+else {
+result.push(left.shift());
+};
+};
+return [...result, ...left, ...right];
+};
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
